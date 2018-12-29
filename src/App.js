415fddc38bd071a27c62498848128421eae58c23
yakubu rufai mahatma <yrufai23@gmail.com>
2018-12-29 16:07:44 +0000

    initial commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,38 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
-import './App.css';
+import './App.scss';
 
 class App extends Component {
+  constructor(props){
+    super(props)
+    this.keyPress = this.keyPress.bind(this);
+    
+  }
+
+  keyPress(key){
+    let style = {
+        background: "#FFF00"
+    }
+    document.querySelector("#Q").style = style
+    console.log(key.key)
+    };
+  
+
   render() {
+    
     return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
+      <div id="drum-machine">
+        <div id="drums">
+          <button className="drum-pad" id="Q" href="#" onKeyPress={this.keyPress} onClick={yes=>{console.log('yes')}}>Q</button>
+          <button className="drum-pad" id="W">W</button>
+          <button className="drum-pad" id="E">E</button>
+          <button className="drum-pad" id="A">A</button>
+          <button className="drum-pad" id="S">S</button>
+          <button className="drum-pad" id="D">D</button>
+          <button className="drum-pad" id="Z">Z</button>
+          <button className="drum-pad" id="X">X</button>
+          <button className="drum-pad" id="C">C</button>
+        </div>
+        <div id="display"></div>
       </div>
     );
   }
