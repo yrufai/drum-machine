56f7216a71a92743b33c407d855a385fb4048482
yakubu rufai mahatma <yrufai23@gmail.com>
2018-12-30 01:15:08 +0000

    audio files shuffled

--- src/App.js
+++ src/App.js
@@ -1,17 +1,12 @@
 import React, { Component } from 'react';
 import './App.scss';
 import AudioFiles from "./AudioFiles";
-import KeyQ from './keyQ';
-import KeyW from './keyW';
 
 class App extends Component {
   constructor(props) {
     super(props)
-    this.keyPress = this.keyPress.bind(this);
-    this.clickedq = this.clickedq.bind(this);
-    this.clickedw = this.clickedw.bind(this);
     this.state = {
-      keys: ["Q","W","E","A","S","D"],
+      keys: ["Q", "W", "E", "A", "S", "D"],
       sounds: []
     }
 
@@ -20,24 +15,44 @@ class App extends Component {
   keyPress(event) {
     console.log(event.key)
     let audio1 = document.getElementById("keyq");
-    if(event.key === "q" || event.key === "Q"){
+    if (event.key === "q" || event.key === "Q") {
       audio1.play();
     }
   };
-  clickedq(){
+  clickedq() {
     let audio1 = document.getElementById("keyq");
     audio1.play();
   }
-  clickedw(){
+  clickedw() {
     let audio1 = document.getElementById("keyw");
     audio1.play();
   }
-  clickede(){
+  clickede() {
     let audio1 = document.getElementById("keye");
     audio1.play();
   }
-  clickeda(){
+  clickeda() {
     let audio1 = document.getElementById("keya");
+    audio1.play();
+  }
+  clickeds() {
+    let audio1 = document.getElementById("keys");
+    audio1.play();
+  }
+  clickedd() {
+    let audio1 = document.getElementById("keyd");
+    audio1.play();
+  }
+  clickedz() {
+    let audio1 = document.getElementById("keyz");
+    audio1.play();
+  }
+  clickedx() {
+    let audio1 = document.getElementById("keyx");
+    audio1.play();
+  }
+  clickedc() {
+    let audio1 = document.getElementById("keyc");
     audio1.play();
   }
 
@@ -45,16 +60,17 @@ class App extends Component {
 
     return (
       <div id="drum-machine" onKeyPress={this.keyPress}>
+      <AudioFiles />
         <div id="drums">
-        <KeyQ  clickedq={this.clickedq} />
-        <KeyW  clickedw={this.clickedw} />
-          <button className="drum-pad" id="E">E</button>
-          <button className="drum-pad" id="A">A</button>
-          <button className="drum-pad" id="S">S</button>
-          <button className="drum-pad" id="D">D</button>
-          <button className="drum-pad" id="Z">Z</button>
-          <button className="drum-pad" id="X">X</button>
-          <button className="drum-pad" id="C">C</button>
+          <button className="drum-pad" id="Q" onClick={this.clickedq}>Q</button>
+          <button className="drum-pad" id="W" onClick={this.clickedw}>W</button>
+          <button className="drum-pad" id="E" onClick={this.clickede}>E</button>
+          <button className="drum-pad" id="A" onClick={this.clickeda}>A</button>
+          <button className="drum-pad" id="S" onClick={this.clickeds}>S</button>
+          <button className="drum-pad" id="D" onClick={this.clickedd}>D</button>
+          <button className="drum-pad" id="Z" onClick={this.clickedz}>Z</button>
+          <button className="drum-pad" id="X" onClick={this.clickedx}>X</button>
+          <button className="drum-pad" id="C" onClick={this.clickedc}>C</button>
         </div>
         <div id="display"></div>
       </div>
--- src/AudioFiles.js
+++ src/AudioFiles.js
@@ -1,23 +1,27 @@
 import React from "react";
-import soundFile3 from "./audio/checkpoint-hit.mp3";
-import soundFile4 from "./audio/clock.mp3";
-import soundFile5 from "./audio/middle-bap.mp3";
+import soundFile1 from "./audio/clap.mp3";
+import soundFile2 from "./audio/upbs18.mp3";
+import soundFile3 from "./audio/base4.mp3";
+import soundFile4 from "./audio/sharp-hard-clap.mp3";
+import soundFile5 from "./audio/pop-clap.mp3";
 import soundFile6 from "./audio/shortsaxish.mp3";
-import soundFile7 from "./audio/smatter4-zap.mp3";
-import soundFile8 from "./audio/tinynote.mp3";
-import soundFile9 from "./audio/upbs3.mp3";
+import soundFile7 from "./audio/real-room-clap.mp3";
+import soundFile8 from "./audio/chimmey.mp3";
+import soundFile9 from "./audio/cymbal8.mp3";
 
 function AudioFiles(props) {
-        return (
-            <div>
-                <audio id="keye"><source src={soundFile3} type="audio/mpeg" ></source></audio>
-                <audio id="keya"><source src={soundFile4} type="audio/mpeg" ></source></audio>
-                <audio id="keys"><source src={soundFile5} type="audio/mpeg" ></source></audio>
-                <audio id="keyd"><source src={soundFile6} type="audio/mpeg" ></source></audio>
-                <audio id="keyz"><source src={soundFile7} type="audio/mpeg" ></source></audio>
-                <audio id="keyx"><source src={soundFile8} type="audio/mpeg" ></source></audio>
-                <audio id="keyc"><source src={soundFile9} type="audio/mpeg" ></source></audio>
-            </div>
-        );
+    return (
+        <div>
+            <audio id="keyw"><source src={soundFile2} type="audio/mpeg" ></source></audio>
+            <audio id="keyq"><source src={soundFile1} type="audio/mpeg" ></source></audio>
+            <audio id="keye"><source src={soundFile3} type="audio/mpeg" ></source></audio>
+            <audio id="keya"><source src={soundFile4} type="audio/mpeg" ></source></audio>
+            <audio id="keys"><source src={soundFile5} type="audio/mpeg" ></source></audio>
+            <audio id="keyd"><source src={soundFile6} type="audio/mpeg" ></source></audio>
+            <audio id="keyz"><source src={soundFile7} type="audio/mpeg" ></source></audio>
+            <audio id="keyx"><source src={soundFile8} type="audio/mpeg" ></source></audio>
+            <audio id="keyc"><source src={soundFile9} type="audio/mpeg" ></source></audio>
+        </div>
+    );
 }
 export default AudioFiles;
