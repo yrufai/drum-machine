fc0dfc3e2e2fc76f8703b2c551882e13383a7866
yakubu rufai mahatma <yrufai23@gmail.com>
2019-01-05 01:56:03 +0000

    modified to look like a piano

--- src/App.js
+++ src/App.js
@@ -1,36 +1,34 @@
-import React, { Component } from 'react';
-import './App.scss';
+import React, { Component } from "react";
+import "./App.scss";
 import AudioFiles from "./AudioFiles";
 import $ from "jquery";
 
-
 class App extends Component {
-
   keyPress(event) {
-    let upCase = event.key.toUpperCase()
+    let upCase = event.key.toUpperCase();
     let getId = document.getElementById(upCase);
     $(getId).toggleClass("active");
 
     if (event.key === "q" || event.key === "Q") {
-      return document.getElementById("keyq").play()
+      return document.getElementById("keyq").play();
     } else if (event.key === "w" || event.key === "W") {
-      return document.getElementById("keyw").play()
+      return document.getElementById("keyw").play();
     } else if (event.key === "e" || event.key === "E") {
-      return document.getElementById("keye").play()
+      return document.getElementById("keye").play();
     } else if (event.key === "a" || event.key === "A") {
-      return document.getElementById("keya").play()
+      return document.getElementById("keya").play();
     } else if (event.key === "s" || event.key === "S") {
-      return document.getElementById("keys").play()
+      return document.getElementById("keys").play();
     } else if (event.key === "d" || event.key === "D") {
-      return document.getElementById("keyd").play()
+      return document.getElementById("keyd").play();
     } else if (event.key === "z" || event.key === "Z") {
-      return document.getElementById("keyz").play()
+      return document.getElementById("keyz").play();
     } else if (event.key === "x" || event.key === "X") {
-      return document.getElementById("keyx").play()
+      return document.getElementById("keyx").play();
     } else if (event.key === "c" || event.key === "C") {
-      return document.getElementById("keyc").play()
+      return document.getElementById("keyc").play();
     } else return null;
-  };
+  }
 
   clickedq() {
     let audio1 = document.getElementById("keyq");
@@ -70,22 +68,47 @@ class App extends Component {
   }
 
   render() {
-
     return (
       <div id="drum-machine" onKeyPress={this.keyPress}>
         <AudioFiles />
         <div id="drums">
-          <button className="drum-pad" id="Q" onClick={this.clickedq}>Q</button>
-          <button className="drum-pad" id="W" onClick={this.clickedw}>W</button>
-          <button className="drum-pad" id="E" onClick={this.clickede}>E</button>
-          <button className="drum-pad" id="A" onClick={this.clickeda}>A</button>
-          <button className="drum-pad" id="S" onClick={this.clickeds}>S</button>
-          <button className="drum-pad" id="D" onClick={this.clickedd}>D</button>
-          <button className="drum-pad" id="Z" onClick={this.clickedz}>Z</button>
-          <button className="drum-pad" id="X" onClick={this.clickedx}>X</button>
-          <button className="drum-pad" id="C" onClick={this.clickedc}>C</button>
+            <div className="upper-drums">
+              <button>press me</button>
+            </div>
+            <div className="lower-drums">
+            <button className="top-buttons">xy</button>
+              <button className="drum-pad" id="Q" onClick={this.clickedq}>
+              Q
+            </button>
+              <button className="drum-pad" id="W" onClick={this.clickedw}>
+              W
+            </button>
+              <button className="drum-pad" id="E" onClick={this.clickede}>
+              E
+            </button>
+              <button className="drum-pad" id="A" onClick={this.clickeda}>
+              A
+            </button>
+              <button className="drum-pad" id="S" onClick={this.clickeds}>
+              S
+            </button>
+              <button className="drum-pad" id="D" onClick={this.clickedd}>
+              D
+            </button>
+              <button className="drum-pad" id="Z" onClick={this.clickedz}>
+              Z
+            </button>
+              <button className="drum-pad" id="X" onClick={this.clickedx}>
+              X
+            </button>
+              <button className="drum-pad" id="C" onClick={this.clickedc}>
+              C
+            </button>
+          </div>
         </div>
-        <div id="display"></div>
+        <div id="display">
+          <button>click me</button>
+        </div>
       </div>
     );
   }
